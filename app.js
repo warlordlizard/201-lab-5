204bed2ab4399cb5b2a6e9837eac5873288f8c50
warlordlizard <alexjbowers88@gmail.com>
2017-12-08 14:30:35 -0800

    multiply function works

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
 
-// TODO: Write your code here
+// TODO: DONE
 function sum(a,b){ //eslint-disable-line
   var theSum = a + b;
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
   return [theSum, message];
 }
 
-// TODO: Here is the test for sum(); uncomment it to run it
+// TODO: DONE
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -30,13 +30,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// TODO: Write your code here
+// TODO: DONE
 function multiply(a,b){ //eslint-disable-line
-
+  var theProduct = a * b
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + theProduct + '.';
+  return [theProduct, message];
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
